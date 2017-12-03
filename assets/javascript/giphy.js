09f13211b74053eb0ae7b7fc2a50261c0bcce8b5
Juanita Pinkney <juanitapinkney@Juanitas-MacAir.attlocal.net>
2017-12-03 11:56:43 -0500

    adds changes

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,78 +1,136 @@
-    //  Declare variables with definitions
-    var buttons = ("unicorns", "fairies", "leprechauns");  
-
-    // Create image tag and store in variable for later
-    var buttons = $("<img>");
-
-    // Store original image URL in variable
-    var imageUrl = response.data.image_original_url;
-
-    // Insert an image to id before page loads
-      $("#buttons").prepend(buttons);
-    });
-
-    // Inserts an on click function that will allowusers to view giphy images when buttons are clicked.
-      $("#buttons").on("click", function() {
-      }
-
-    // Gives the path of image src, alt gives description of image
-        buttons.attr("src", imageUrl);
-        buttons.attr("alt", "buttons image");
-
-    // Function for displaying giphy images
-      function renderButtons() {
+// Initial array of beaches
+var beach = ["Mexico", "Jamaica", "Carribean", "Hawaii"];
 
 
-     $("#buttons").on("click", function() {
-    // Stores a random static  API key.
-      var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=RrOdcCds1SXzLGzzVKauPSyeG1jwlTtI&q=unicorns&limit=10&offset=0&rating=G&lang=en";
-     
-    //Creating an array of strings, each one related to a topic that interests Save it to a variable called `buttons`.  make a list to your own liking.
+// DisplayBeachImage function re-renders the HTML to display the appropriate content
+	function displayBeachImage() {
 
-    // Performing GET requests to the OMDB API and logging the responses to the console
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
 
-    .done(function(response) {
-      console.log(response);
-    }); 
+// Storing a random  API key.
+var beaches = $(this).attr("data-name");
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=LtNoa2bzwYtqkpZxaDXpeu2arbF0VDx3&q=q&limit=10&offset=0&rating=G&lang=en";
 
-    // // Pausing the giphy images on click.
-    //  var state = $ (this).attr("data-state");
 
-    //    console.log (data-state)
 
-    //    if (state === "still"); {
-    //     $(this).attr( "src", $(this).attr( "data-animate"));
-    //    } else {
-    //     $(this).attr( "src", $(this).attr( "data-still"));
-    //    }
-    // });
+// Creating an AJAX call for the specific beach button being clicked
+$.ajax({
+url: queryURL ,
+method: "GET"
+}).done(function(response)) {
+}
 
-      // Inserting for loop to increment and image per topic string
+// Creating a div to hold the beach
+var beachDiv = $("<div class='beach'>");
 
-      //   for (var i = 0; i < buttons.length; i++) {
+// Creating an element to have the rating displayed
+var beach = $("<im>").text("Rating: " + rating);
 
-      // // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
+// Storing the rating data
+var rating = response.Rated;
 
-      //     var a = $("<button>");
+// Displaying the rating
+	beachDiv.append(beach);
 
-      // // Adding a class
-      //     a.addClass("buttons");
+// Storing the beach
+var beach = response.Beach;
 
-      // // Adding a data-attribute with a value of the topic at index i
-      //     a.attr("data-name", buttons[i]);
+// Creating an element to hold the beach
+var pThree = $("<p>").text("beach: " + beach);
 
-      // // Providing the button's text with a value of the topic at index i
-      //     a.text(buttons[i]);
+// Appending the beach
+	beachDiv.append(pThree);
 
-      // Adding the button to the HTML
-      //     $("#buttons").append(a);
-      //   }
-      // }
-          console.log(unicorns[0]);
-          console.log(fairies[1]);
-          console.log(leprechauns[2]);
-  
+// Retrieving the URL for the image
+var imgURL = response.BeachImage;
+
+// Creating an element to hold the image
+var image = $("<img>").attr("src", imgURL);
+
+// Appending the image
+  beachDiv.append(image);
+
+// Putting the entire beach above the previous beach
+$("#beach-view").prepend(beachDiv);
+});
+
+// Function for displaying beach data
+  function renderButtons() {
+
+  }
+
+// Deleting the beach prior to adding new beach (this is necessary otherwise you will have repeat buttons)
+$("#buttons-view").empty();
+
+
+// Store original image URL in variable
+var imageUrl = response.data."https://api.giphy.com/v1/gifs/search?q=api_key=RrOdcCds1SXzLGzzVKauPSyeG1jwlTtI&q=beaches&limit=5&offset=0&rating=G&lang=en";
+
+
+// Gives the path of image src, alt gives description of image
+buttons.attr("src", imageUrl);
+buttons.attr("alt", "buttons image");
+
+
+// Function for displaying beach data
+function renderButtons() {
+
+// Deleting the beach buttons prior to adding new beach buttons
+// (this is necessary otherwise we will have repeat buttons)
+$("#beach-view").empty();
+
+// This function handles events where one button is clicked
+$("#add-beach").on("click", function(event) {
+
+// event.preventDefault() prevents the form from trying to submit itself; using a form so that the user can hit enter instead of clicking the button if they want
+event.preventDefault();
+
+// Looping through the array of beach
+    for (var i = 0; i < beach.length; i++) {
+
+// This function handles events where a beach button is clicked
+$("#add-beach").on("click", function(event) {
+event.preventDefault();
+
+
+// Adding a click event listener to all elements with a class of "beach"
+$(document).on("click", ".beach", displayBeachImage);
+
+// Calling the renderButtons function to display the intial buttons
+	renderButtons();
+});
+
+// This line grabs the input from the textbox
+var beach = $("#beach-input").val().trim();
+
+// Adding beach from the textbox to our array
+	beach.push(beach);
+
+// Calling renderButtons which handles the processing of our beach array
+renderButtons();
+});
+
+// Calling the renderButtons function at least once to display the initial list of beachs
+renderButtons();
+
+// Then dynamicaly generating buttons for each beach in the array. This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+var a = $("<button>");
+
+// Adding a class of beach to our button
+	a.addClass("beach");
+
+// Adding a data-attribute
+	a.attr("data-name", beach[i]);
+
+// Providing the initial button text
+	a.text(beach[i]);
+
+// Adding the button to the buttons-view div
+$("#buttons-view").append(a);{
+
+}
+
+
+.done(function(response) {
+
+console.log(response);
+}); 
