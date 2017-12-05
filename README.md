ac3b412b1cc5a7ad52b5e41b3f817f2c3a47f9fe
Juanita Pinkney <juanitapinkney@Juanitas-MacAir.attlocal.net>
2017-12-04 22:06:32 -0500

    adds changes

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# giphyappy
+# giphyappy 
+
+ The idea of creating my giphy webpage was very exciting and I had high hopes of delivering a great and functional giphy webpage, on time. Apparently, my understanding of the use of ajax/json is not at a level as I anticipated. 
+
+ My purpose and plan in completing this homework assignment was/is to have Giphy/API  images and data displayed upon my page, along with an additional button to be display that would be prompted by the user’s search. In once instance, I, for certain, had the additional button displayed on the page; however, that working code has mysteriously disappeared from my html and js files.
+
+ Looking at my js file, I'm certain that my code and placement of code is, indeed, correct, including the closures and search parameters. Unfortunately, I was unsuccessful at pushing the API/Giphy data and images to the call. 
+
+ I will desperately, continue to debug and find a solution.
+
+ Thank you for your consideration.
+
--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,44 +1,81 @@
 // Initial array of beaches
-var beaches = ["Mexico", "Jamaica", "Carribean", "Hawaii", "Aruba", "Bora Bora", "Malibu", "Key Largo"];
-$"buttton"
-// Adding a click event listener to all elements with a class of "beach"
-$(document).on("click", ".beach" "displayBeachImage");{
+var beaches = ["Mexico", "Jamaica", "Carribean", "Hawaii", "Aruba", "Bora Bora", "Belize", "Key Largo"];
 
-}
-
-// Storing a random  API key.
-var beaches = $(this).attr("beaches-name");{
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=beautiful+calm+beaches&api_key=LtNoa2bzwYtqkpZxaDXpeu2arbF0VDx3&&limit=10&offset=0&rating=G&lang=en&fmt=json";
-}
-// Creating an AJAX call for the specific beach button being clicked
-$.ajax({
-method: "GET",
-url:queryURL,
-})
-
-.done(function(json) {
-
-
-var url =json.data [i].images.downsized.url;
-
-console.log (json.data);
-});
-
-// Deleting the beach buttons prior to adding new beach buttons
-// (this is necessary otherwise we will have repeat buttons)
-$("#beaches-view").empty();{
-}
 
 // This function handles events where one button is clicked
 $("#add-beaches").on("click", function(event) {
-
 event.preventDefault();
 
-var data = $("#beach-input").val();
 
-beaches.push (data);
+// Adding a click event listener to all elements with a class of "beach"
+$(document).on("click", ".beach", displayGifs);
 
-	
+
+$("buttton").click([beaches.length; i++]);
+
+// Calling the renderButtons function to display the initial buttons
+renderButtons();
+
+
+// Adding the beaches from the textbox to our array
+beaches.push(gifs);
+
+
+// Deleting the buttons prior to adding new beaches
+// (this is necessary otherwise you will have repeat buttons)
+$("#buttons-view").empty();
+
+});
+
+
+
+console.log (beaches[beaches.length]);
+
+
+// Function for displaying the beaches info
+$(document).on("click", ".beaches", displayGifsInfo);
+
+// Storing my API key.
+var beaches = $(this).attr("beaches-name");{
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=beautiful+calm+beaches&api_key=LtNoa2bzwYtqkpZxaDXpeu2arbF0VDx3&&limit=10&offset=0&rating=G&lang=en&fmt=json";
 }
 
-console.log ()
+
+// Creating an AJAX call for the specific beach button being clicked
+$.ajax({
+url:queryURL,
+method: "GET",
+}).done(function(response) {
+html = json.data[0].images
+
+$("#beaches-view").text(json.gif(response));
+renderButtons();
+});
+
+console.log (data);
+
+
+// Looping through the array of beachess
+for (var i = 0; i < beaches.length; i++) {
+
+
+// Then dynamically generating buttons for each beaches in the array
+// This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+var a = $("<button>");
+
+
+// Adding a class of beaches to our button
+a.addClass("beaches");
+
+
+// Adding a data-attribute
+a.attr("data-name", beaches[i]);
+
+
+// Providing the initial button text
+a.text(gifs[i]);
+
+
+// Adding the button to the buttons-view div
+$("#buttons-view").append(a);
+};
