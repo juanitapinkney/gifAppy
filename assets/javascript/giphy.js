f7a5d022768391fa6bcbf5971ba783d3c18de63a
Juanita Pinkney <juanitapinkney@Juanitas-MacAir.attlocal.net>
2017-12-07 00:08:57 -0500

    adds updates

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,5 +1,5 @@
 // Initial array of beaches
-var beaches = ["Mexico", "Jamaica", "Carribean", "Hawaii", "Aruba", "Bora Bora", "Belize", "Key Largo"];
+var beaches = ["Mexico", "Jamaica", "Hawaii", "Aruba", "Belize", "Brazil", "Cape Town", "Bora Bora", "Key Largo"];
 
 $( document ).ready(function() {
 
@@ -14,8 +14,8 @@ $("#add-beaches").on("click", function(event) {
 event.preventDefault();
 });
 
-function ajaxCall () {
-	var beaches = $(this).attr("beaches-name");
+// function ajaxCall () {
+// 	var beaches = $(this).attr("beaches-name");
 	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + beaches +  "&api_key=LtNoa2bzwYtqkpZxaDXpeu2arbF0VDx3&&limit=10&offset=0&rating=G&lang=en&fmt=json";
 	
 
@@ -24,73 +24,46 @@ function ajaxCall () {
 	$.ajax({
 		url:queryURL,
 		method: "GET",
-	}).done(function(response) {
+	})
+
+	.done(function(response) {
 		console.log(response)
+	})
 
 		for (var i = 0; i < response.data.length; i++) {
-			console.log( response.data[i] );
-		}
-	});
-}
-// // Adding a click event listener to all elements with a class of "beach"
-// $(document).on("click", ".beach", displayGifs);
+
+			console.log(response.data[i].images.downsized.url);
+			console.log(response.data[i].rating);
+			console.log(response.data[i]);
+
+
+        // Saving the image_original_url property
+        var imageUrl = response.data.images.downsized.url;
+
+        // Creating and storing an image tag
+        var beachImage = $("<img>");
+
+        // Setting the beachImage src attribute to imageUrl
+        beachImage.attr("src", imageUrl);
+        beachImage.attr("alt", "beach image");
+
+        // Prepending the beachImage to the images div
+        $("#images").prepend(beachImage);
+      };
 
 
 
-
-// // Calling the renderButtons function to display the initial buttons
-// renderButtons();
-
-
-// // Adding the beaches from the textbox to our array
-// beaches.push(gifs);
-
-
-// // Deleting the buttons prior to adding new beaches
-// // (this is necessary otherwise you will have repeat buttons)
-// $("#buttons-view").empty();
-
-
-
-
-// //  Attempt to console the entire array, no success.
-// console.log (beaches[beaches.length]);
-
-
-// // Function for displaying the beaches info
-// $(document).on("click", ".beaches", displayGifsInfo);
-
-
-// Defining my API key.
-
-// //Attempting to call the json data to be displayed
-// $("#beaches-view").text(json.gif(response));
-// renderButtons();
-// });
-
-
-
-// // Looping through the array of beaches
-// for (var i = 0; i < beaches.length; i++) {
-
-
-// Generating buttons for each beach in the array
-// This code creates jQuery beginning and end tag.
-var a = $("<button>");
-
-
-// Adding a class of beaches to the button
-a.addClass("beaches");
-
-
-// // Adding a data-attribute
-// a.attr("data-name", beaches[i]);
-
-
-// // Providing the initial button text
-// a.text(gifs[i]);
-
-
-// Adding the button to the buttons-view div
-$("#buttons-view").append(a);
-
+          $(".gif").on("click", function() {
+      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+      var state = $(this).attr("data-state");
+      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+      // Then, set the image's data-state to animate
+      // Else set src to the data-still value
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+    });
