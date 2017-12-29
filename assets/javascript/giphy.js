52b2dec6fcdb301a3dc9bdf33e9f246ce9f8f2e4
Juanita Pinkney <juanitapinkney@Juanitas-MacAir.attlocal.net>
2017-12-29 03:14:38 -0500

    changes

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,7 +1,8 @@
 var GiphyAPI = LtNoa2bzwYtqkpZxaDXpeu2arbF0VDx3;
 var url = json.data[0].images.downsized.url;
 // Initial array of beaches
-var beaches = ["Mexico", "Jamaica", "Hawaii", "Aruba", "Belize", "Brazil", "Cape Town", "Bora Bora", "Key Largo"];
+var beaches = ["Mexico", "Jamaica", "Hawaii", "Aruba", "Belize", "Brazil", "Cape Town", "Bora Bora",
+  "Key Largo"];
 $(document).ready(function() {
     $("button").click(function() {
         ajaxCall();
@@ -19,6 +20,59 @@ $(document).on("click", "img", function() {
         img.attr("src", img.attr("data-original-url"));
     }
 });
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + beaches +
+    "&api_key=LtNoa2bzwYtqkpZxaDXpeu2arbF0VDx3&&limit=10&offset=0&rating=G&lang=en&fmt=json";
+$.ajax({
+    url: queryURL,
+    method: 'GET'
+    // call back function
+}).done(function(response) {
+    console.log(response);
+    // clear the images since I have new images
+    $("#images").empty();
+    // to use the data from giphy, I need to write code in the done callback
+    for (var i = 0; i < response.data.length; i++) {
+        console.log(response.data[i].rating);
+        // console log to find info on still and animated
+        console.log(response.data[i].images.original_still.url);
+        console.log(response.data[i].images.original.url);
+        // create new image tag for the images to be called to the page
+        var imageAndRating = $("<div>"); // create div
+        var rating = $("<p>"); // create paragraph inside div to contain both rating and image
+        rating.html("Rating: " + response.data[i].rating); // HTML rating
+        imageAndRating.append(rating); // append the rating and imageandrating div together
+        var newImage = $("<img>"); // create image tag for images to prepared to display
+        newImage.attr("src", response.data[i].images.original_still.url);
+        newImage.attr("data-original-url", response.data[i].images.original.url);
+        newImage.attr("data-still-url", response.data[i].images.original_still.url);
+        imageAndRating.append(newImage);
+        // add image to page
+        $("#images").append(imageAndRating);
+    }
+});
+// Event listener for our beach-button
+$("#beach-button").on("click", function() {
+    // Storing our giphy API URL for a random beach image
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + beaches +
+        "&api_key=LtNoa2bzwYtqkpZxaDXpeu2arbF0VDx3&&limit=10&offset=0&rating=G&lang=en&fmt=json";
+    // Perfoming an AJAX GET request to our queryURL
+    $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        // After the data from the AJAX request comes back
+        .done(function(response) {
+            // Saving the image_original_url property
+            var imageUrl = response.data.image_original_url;
+            // Creating and storing an image tag
+            var beachImage = $("<img>");
+            // Setting the beachImage src attribute to imageUrl
+            beachImage.attr("src", imageUrl);
+            beachImage.attr("alt", "beach image");
+            // Prepending the beachImage to the images div
+            $("#images").prepend(beachImage);
+        });
+});
 // This function handles events where one button is clicked
 $("#add-beaches").on("click", function(event) {
     event.preventDefault();
@@ -26,56 +80,24 @@ $("#add-beaches").on("click", function(event) {
 $("#addSearchBtn").on("click", function() {
     // user function to add button to list of buttons
     // create new button
-    var newBtn = $("<button>");
+    var newBtn = $(" < button > ");
     var searchText = $("#searchText").val();
     newBtn.text(searchText);
     // prepend to body
     $(".container").prepend(newBtn);
 });
-// function ajaxCall () {
-//  var beaches = $(this).attr("beaches-name");
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + beaches + "&api_key=LtNoa2bzwYtqkpZxaDXpeu2arbF0VDx3&&limit=10&offset=0&rating=G&lang=en&fmt=json";
-$.ajax({
-url: queryURL,
-method: 'GET'
-// call back function
-}).done(function(response) {
-console.log(response);
-// clear the images since I have new images
-$("#images").empty();
-// to use the data from giphy, I need to write code in the done callback
-for (var i = 0; i < response.data.length; i++) {
-    console.log(response.data[i].rating);
-    // console log to find info on still and animated
-    console.log(response.data[i].images.original_still.url);
-    console.log(response.data[i].images.original.url);
-    // create new image tag for the images to be called to the page
-    var imageAndRating = $("<div>"); // create div
-    var rating = $("<p>"); // create paragraph inside div to contain both rating and image
-    rating.html("Rating: " + response.data[i].rating); // HTML rating
-    imageAndRating.append(rating); // append the rating and imageandrating div together
-    var newImage = $("<img>"); // create image tag for images to prepared to display
-    newImage.attr("src", response.data[i].images.original_still.url);
-    newImage.attr("data-original-url", response.data[i].images.original.url);
-    newImage.attr("data-still-url", response.data[i].images.original_still.url);
-    imageAndRating.append(newImage);
-    // add image to page
-    $("#images").append(imageAndRating); 
-}
-});
-$(".gif").on("click", function() {
-    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-    var state = $(this).attr("data-state");
-    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-    // Then, set the image's data-state to animate
-    // Else set src to the data-still value
-    if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-    } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-    }
-});
-// Calling the renderButtons function at least once to display the initial list of movies
-renderButtons();
+
+
+$("#searchResults").append(' Rating: '+rating+'');
+
+
+
+
+
+
+
+
+
+
+
+
